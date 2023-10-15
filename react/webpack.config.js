7e7c60fd005f94a61157daa38503f152ef3c0dba
xiaohai-huang <jeromehuang18@gmail.com>
2023-10-15 01:36:03 -0600

    chore: Add image compression

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,4 +1,30 @@
+const { merge } = require("webpack-merge");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+
 module.exports = (env, baseConfig) => {
-  baseConfig.output.assetModuleFilename = "static/media/[name].[hash][ext]";
-  return baseConfig;
+  const config = merge(baseConfig, {
+    output: {
+      assetModuleFilename: "static/media/[name].[hash][ext]",
+    },
+    optimization: {
+      minimize: env === "production",
+      minimizer: [
+        "...",
+        new ImageMinimizerPlugin({
+          minimizer: {
+            implementation: ImageMinimizerPlugin.sharpMinify,
+            options: {
+              encodeOptions: {
+                jpeg: {
+                  quality: 80,
+                },
+              },
+            },
+          },
+        }),
+      ],
+    },
+  });
+
+  return config;
 };
