499111280e5d426445fc299f102459c1d438c7fc
xiaohai-huang <jeromehuang18@gmail.com>
2023-10-24 01:56:00 -0600

    chore: Run patch in background

--- Assets/Scripts/DiffTool/YZQBSDiff.cs
+++ Assets/Scripts/DiffTool/YZQBSDiff.cs
@@ -5,15 +5,15 @@ namespace DiffTool
 {
     class YZQBSDiff : IDiffTool
     {
-        public Task<bool> ApplyPatch(string oldFilePath, string patchFilePath, string outputFilePath)
+        public async Task<bool> ApplyPatch(string oldFilePath, string patchFilePath, string outputFilePath)
         {
-            AndroidJavaObject diffTool = new AndroidJavaObject("com.example.myappupdatemanager.DiffTool");
+            using AndroidJavaObject diffTool = new AndroidJavaObject("com.example.myappupdatemanager.DiffTool");
             // Debug.Log("Before call Patch");
-            int result = diffTool.Call<int>("Patch", oldFilePath, patchFilePath, outputFilePath);
+            int result = await Task.Run(() => diffTool.Call<int>("Patch", oldFilePath, patchFilePath, outputFilePath));
             Debug.Log("[YZQBSDiff] result: " + result);
 
             // Debug.Log("After call Patch");
-            return Task.FromResult(result == 0);
+            return result == 0;
         }
     }
 }
