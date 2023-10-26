0a76cb33a1a6ef815e555c199378d47c0b1a8b6b
xiaohai-huang <jeromehuang18@gmail.com>
2023-10-26 10:16:08 -0600

    Add AndroidCodeRunner

---
+++ Assets/Scripts/AndroidCodeRunner.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class AndroidCodeRunner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Code Runner");
+    }
+
+}
