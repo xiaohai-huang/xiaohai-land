041b44fd17d7b6ec51d5d23b1a7755b5be43f435
xiaohai-huang <jeromehuang18@gmail.com>
2023-10-31 12:52:42 +0800

    feat: Add Champion Select page

---
+++ react/src/components/Champion/ChampionIcon.tsx
@@ -0,0 +1,61 @@
+type ChampionIconProps = {
+  id: number;
+  name: string;
+  img: string;
+  size: number;
+  selected: boolean;
+  onClick: (id: number) => void;
+};
+
+const borderRadius = "10%";
+
+function ChampionIcon({
+  id,
+  name,
+  img,
+  size,
+  selected,
+  onClick = () => {},
+}: ChampionIconProps) {
+  const selectedEffect = (
+    <view
+      style={{
+        width: `${size}px`,
+        height: `${size}px`,
+        border: "1.5px solid yellow",
+        position: "absolute",
+        left: 0,
+        top: 0,
+        borderRadius,
+        backgroundColor: "rgba(255,242,0,0.03)",
+      }}
+    />
+  );
+  return (
+    <view style={{ width: size }} onClick={() => onClick(id)}>
+      <view
+        style={{
+          width: `${size}px`,
+          height: `${size}px`,
+          borderRadius,
+          overflow: "hidden",
+        }}
+      >
+        <image source={img} />
+      </view>
+      {selected && selectedEffect}
+      <text
+        style={{
+          textAlign: "center",
+          marginTop: "0.1rem",
+          fontSize: "0.8rem",
+          color: selected ? "white" : "gray",
+        }}
+      >
+        {name}
+      </text>
+    </view>
+  );
+}
+
+export default ChampionIcon;
--- react/src/pages/champion-select/index.tsx
+++ react/src/pages/champion-select/index.tsx
@@ -2,73 +2,52 @@ import { useState } from "react";
 import { useNavigate } from "src/components/MiniRouter/index";
 
 import myImage1 from "src/assets/images/backgrounds/bg-01.jpg";
-import myImage2 from "src/assets/images/backgrounds/bg-02.jpg";
-
+import ChampionIcon from "src/components/Champion/ChampionIcon";
+import useChampions from "src/hooks/useChampions";
+import styles from "./index.module.scss";
 function Page() {
   const navigate = useNavigate();
-  const [show, setShow] = useState(false);
+  const { champions } = useChampions();
+  const [selectedChampion, setSelectedChampion] = useState(-1);
 
   return (
     <view
-      className="champion-select"
+      className={styles.page}
       style={{
-        width: "100%",
-        height: "100%",
         backgroundImage: `url(${myImage1})`,
       }}
     >
-      '/champion-select' page
-      <button
-        onClick={() => {
-          navigate("/");
-        }}
-      >
-        Go back to /
-      </button>
-      <Test />
-      <view>
-        <button onClick={() => setShow((prev) => !prev)}>Show</button>
-        {show && <image src={myImage2} />}
+      {/* <text>'/champion-select' page</text> */}
+      <scroll className={styles.listScroll}>
+        <view className={styles.list}>
+          {champions.slice(0, 19).map((champion) => (
+            <ChampionIcon
+              key={champion.id}
+              id={champion.id}
+              name={champion.name}
+              img={champion.img}
+              size={64}
+              selected={champion.id === selectedChampion}
+              onClick={(id) => {
+                setSelectedChampion(id);
+              }}
+            />
+          ))}
+        </view>
+      </scroll>
+      <view className={styles.previewSection}>champion model</view>
+      <view className={styles.rightSection}>
+        <button
+          style={{ width: "100px", backgroundColor: "lightblue" }}
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          取消
+        </button>
       </view>
     </view>
   );
 }
 
-const Test = () => {
-  return (
-    <div>
-      <div
-        style={{ height: "200px", display: "flex", flexDirection: "column" }}
-      >
-        <div style={{ height: "20px" }}>Header</div>
-        <scroll
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            gap: "1rem",
-          }}
-        >
-          <Box />
-          <Box />
-          <Box />
-          <Box />
-        </scroll>
-        <div style={{ height: "20px" }}>Bottom</div>
-      </div>
-    </div>
-  );
-};
-
-function Box() {
-  return (
-    <div
-      style={{
-        width: "64px",
-        height: "64px",
-        backgroundColor: "lightblue",
-        flexShrink: 0,
-      }}
-    />
-  );
-}
 export default Page;
