5f79e520752801fc02c047e7e08862bf6f12f92b
xiaohai-huang <jeromehuang18@gmail.com>
2023-11-10 08:41:52 -0700

    feat: Add switch UI source capability

--- react/src/pages/settings/tabs/BasicTab.tsx
+++ react/src/pages/settings/tabs/BasicTab.tsx
@@ -1,32 +1,78 @@
 import { useState } from "react";
+import classNames from "classnames";
+import type { UnityEngine } from "@reactunity/renderer";
+
 import Select from "src/components/Select";
 import styles from "./BasicTab.module.scss";
 
+const PRODUCTION_URL = process.env.REACT_APP_PRODUCTION_URL;
+
 type ScriptSource = "production" | "local";
 
-function HandleRestart(source: ScriptSource) {
+function HandleRestart(type: ScriptSource, source?: string) {
+  // Update source
+  switch (type) {
+    case "local": {
+      Renderer.Source.SourcePath = source;
+      break;
+    }
+    case "production": {
+      Renderer.Source.SourcePath = PRODUCTION_URL;
+      break;
+    }
+  }
+
   Renderer.Invoke("Render", 0);
 }
 
 function BasicTab() {
-  const [scriptSource, setScriptSource] = useState<ScriptSource>("production");
+  const [scriptSourceType, setScriptSourceType] = useState<ScriptSource>(() =>
+    Context.Source.SourcePath === PRODUCTION_URL ? "production" : "local"
+  );
+  const [localScriptSource, setLocalScriptSource] = useState(
+    "http://192.168.31.224:8080/index.js"
+  );
 
   return (
     <view className={styles.container}>
-      <view className={styles.item}>
+      <view className={classNames(styles.item, styles.scriptSource)}>
         <view className={styles.label}>Script Source</view>
         <view className={styles.content}>
-          <Select value={scriptSource} onChange={(v) => setScriptSource(v)}>
-            <Select.Option value="production">Production</Select.Option>
-            <Select.Option value="local">Local Dev</Select.Option>
-          </Select>
+          <view className={styles.select}>
+            <Select
+              value={scriptSourceType}
+              onChange={(v) => setScriptSourceType(v)}
+            >
+              <Select.Option value="production">Production</Select.Option>
+              <Select.Option value="local">Local Dev</Select.Option>
+            </Select>
+          </view>
+          {scriptSourceType === "local" && (
+            <input
+              className={styles.input}
+              value={localScriptSource}
+              onChange={(e) => {
+                setLocalScriptSource(e.newValue);
+              }}
+              onAttachToPanel={(e) => {
+                const element =
+                  e.currentTarget as UnityEngine.UIElements.TextField;
+                const textInput = element.ElementAt(0);
+                StyleHelper.SetPadding(textInput, "all", 0);
+                StyleHelper.SetAlignItems(textInput, "center");
+              }}
+            />
+          )}
         </view>
       </view>
       <view className={styles.bottom}>
+        <view className={styles.sourcePath}>
+          current source: {Context.Source.SourcePath}
+        </view>
         <view className={styles.restart}>
           <button
             className={styles.btn}
-            onClick={() => HandleRestart(scriptSource)}
+            onClick={() => HandleRestart(scriptSourceType, localScriptSource)}
           >
             Restart
           </button>
