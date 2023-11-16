c1875ee73098ae39873c87cb3d97dfe91e59e1fa
xiaohai-huang <jeromehuang18@gmail.com>
2023-11-16 08:52:00 -0700

    chore: Update test page

--- react/src/pages/test/index.tsx
+++ react/src/pages/test/index.tsx
@@ -1,32 +1,23 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "src/components/MiniRouter";
 import Image from "src/components/Image";
 
 import styles from "./index.module.scss";
+import type { ReactUnity } from "@reactunity/renderer";
 
 function Page() {
   const navigate = useNavigate();
   const [type, setType] = useState("contain");
+  const inputRef = useRef<ReactUnity.UIToolkit.TextFieldComponent>(null);
+  const [show, setShow] = useState(false);
   return (
     <view
-      className="background-image"
+      className={styles.container}
       style={{
         width: "100%",
         height: "100%",
-        backgroundColor: "lightblue",
       }}
     >
-      <button
-        onClick={async () => {
-          // const res = await fetch(
-          //   "https://game.gtimg.cn/images/yxzj/img201606/heroimg/112/112.jpg"
-          // );
-          // console.log(JSON.stringify(res.headers));
-          console.log(Context);
-        }}
-      >
-        Download img
-      </button>
       <button
         onClick={() => {
           navigate("/");
@@ -34,59 +25,34 @@ function Page() {
       >
         Go back to /
       </button>
-      <Image
-        style={{
-          objectFit: type,
-          width: "100%",
-          height: "200px",
+      <input
+        ref={inputRef}
+        label="normal"
+        onKeyDownCapture={(e) => {
+          console.log("keydown");
         }}
-        src="https://game.gtimg.cn/images/yxzj/img201606/heroimg/112/112.jpg"
+        onClick={() => setShow(true)}
       />
-      <view
-        style={{
-          marginTop: "1rem",
-          textAlign: "center",
-          fontSize: "1.5rem",
+      <input
+        label="hideMobileInput"
+        onKeyDownCapture={(e) => {
+          console.log("keydown");
         }}
-      >
-        <text>object-fit:{type}</text>
-      </view>
-      <view
-        style={{
-          margin: "auto",
-          display: "flex",
-          flexDirection: "row",
-          fontSize: "2rem",
+        onAttachToPanel={(_, { Element }) => {
+          Element.hideMobileInput = true;
         }}
-      >
-        <view className={styles.toggle} style={{ marginRight: "2rem" }}>
-          contain
-          <toggle
-            value={type === "contain"}
-            onChange={(e) => {
-              e.newValue && setType("contain");
-            }}
-          />
-        </view>
-        <view className={styles.toggle} style={{ marginRight: "2rem" }}>
-          cover
-          <toggle
-            value={type === "cover"}
-            onChange={(e) => {
-              e.newValue && setType("cover");
-            }}
-          />
-        </view>
-        <view className={styles.toggle}>
-          fill
-          <toggle
-            value={type === "fill"}
-            onChange={(e) => {
-              e.newValue && setType("fill");
-            }}
-          />
-        </view>
-      </view>
+      />
+      {show && (
+        <view
+          onClick={() => setShow(false)}
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0,0,0,0.5)",
+          }}
+        />
+      )}
     </view>
   );
 }
