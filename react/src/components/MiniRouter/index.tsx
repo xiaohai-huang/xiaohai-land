abbdf4cce3c1ba79d998c311a20d3807e50a1767
xiaohai-huang <jeromehuang18@gmail.com>
2023-11-17 17:13:51 +0800

    fix: Update opacity if the route is not match

--- react/src/components/MiniRouter/index.tsx
+++ react/src/components/MiniRouter/index.tsx
@@ -44,6 +44,8 @@ export function Route({ path, element, keepAlive = false }: RouteProps) {
       className="mini-router-route"
       style={{
         visibility: path === location ? "visible" : "hidden",
+        // hide element which has transform set
+        opacity: path === location ? 1 : 0,
         position: "absolute",
         left: 0,
         top: 0,
